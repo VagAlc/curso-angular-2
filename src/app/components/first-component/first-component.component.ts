4181bea0dc35a8f0e3741377c87dab1d241ecdbf
Vagner Alcantara <vag_alc@hotmail.com>
2023-01-18 18:25:36 -0300

    Aula 5

--- src/app/components/first-component/first-component.component.ts
+++ src/app/components/first-component/first-component.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-first-component',
   templateUrl: './first-component.component.html',
   styleUrls: ['./first-component.component.css']
 })
-export class FirstComponentComponent {
-
+export class FirstComponentComponent implements OnInit {
+  name: string = 'Vagner';
+  age = 30;
+  job = 'Programador';
+  hobbie = ['Correr', ' Jogar', ' Estudar'];
+  car = {
+    name: "Duster",
+    year: 2016,
+  };
+  constructor () {}
+  ngOnInit(): void {
+    
+  }
 }
