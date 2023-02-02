dd71133f63cefba2d603e7819be1a929447460d1
Vagner Alcantara <vag_alc@hotmail.com>
2023-02-02 09:37:24 -0300

    Aula 17 e 18 Router e Requisições HTTP

--- src/app/components/list-render/list-render.component.ts
+++ src/app/components/list-render/list-render.component.ts
@@ -10,15 +10,13 @@ import { ListService } from 'src/app/services/list.service';
   styleUrls: ['./list-render.component.css']
 })
 export class ListRenderComponent implements OnInit {
-  animals: Animal[] = [
-    {name: 'Turca', type: 'Dog', age: 4},
-    {name: 'Tom', type: 'Cat', age: 10},
-    {name: 'Frida', type: 'Dog', age: 5},
-    {name: 'Bob', type: 'Horse', age: 1},
-  ];
+  animals: Animal[] = [];
+
   animalDetails = '';
 
-  constructor (private listService: ListService)  { }
+  constructor (private listService: ListService)  { 
+    this.getAnimals()
+  }
   
   
   ngOnInit(): void {
@@ -34,4 +32,9 @@ this.animalDetails = `O pet ${animal.name} tem ${animal.age} anos!`;
     console.log('Removendo Animal...')
     this.animals = this.listService.remove(this.animals, animal);
   }
-}
+
+  getAnimals(): void {
+    this.listService.getAll().subscribe((animals) => (this.animals = animals));
+    }
+    
+    }
